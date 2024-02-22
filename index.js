fef9ad9c54a16485bd745b04b4a4b46d21169247
Dennis <dennismusembi344@gmail.com>
2024-02-22 22:35:04 +0300

    done2

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-// Your code goes here
+// // Your code goes here
+// document.addEventListener("DOMContentLoaded",()=>{
+//     let pNew=document.querySelector("p")
+//     pNew.textContent="This is really cool!"
+// })
 document.addEventListener("DOMContentLoaded",()=>{
-    let pNew=document.querySelector("p")
-    pNew.textContent="This is really cool!"
+    let p=document.querySelector('#text')
+    p.textContent="This is really cool!"
 })
